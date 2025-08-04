f94e89a6742be7beea46d4b53ece494b3933d1d6
GuilFran Aspire7 <guilfranca27@gmail.com>
2025-08-04 16:01:11 -0300

    Aula 3 - Inicializada, 02 finalizada

--- jogoteca.py
+++ jogoteca.py
@@ -12,8 +12,14 @@ app = Flask(__name__)
 def ola():
     jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
     jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 
-    listaJogos = [jogo1, jogo2]
+    listaJogos = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo = 'Jogos', jogos = listaJogos)
+
+@app.route("/novo")
+def novo():
+    return render_template('novo.html', titulo = 'Novo Jogo')
+
 
 app.run(debug=True)
